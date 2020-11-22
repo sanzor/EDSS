38740e687870b7ad977797926e4732a01e7db05a
Adrian Bercovici <adriansan_93@yahoo.com>
2020-11-22 21:14:28 +0200

    changed sup

--- src/wsup.erl
+++ src/wsup.erl
@@ -1,14 +1,17 @@
 -module(wsup).
 -behaviour(supervisor).
--export([init/0,start/0]).
+-export([init/1,start/0]).
+-import(gen_tcp,[listen/1]).
 
 -define(NAME,?MODULE).
+-define(PORT,8300).
 
 start()->
-    supervisor:start_link({loocal,?NAME},[]).
+    supervisor:start_link({local,?NAME},?NAME,[]).
 
-init()->
-    ListenSock=gen_tcp:listen(8300),
+init(Args)->
+    Options=[inet,]
+    ListenSock=gen_tcp:listen(?PORT,[]),
     SupFlags=#{
         strategy=>simple_one_for_one,
         intensity=>5,
@@ -21,3 +24,4 @@ init()->
         type=>worker
         },
     {ok,{SupFlags,ChildSpec}}.
+
