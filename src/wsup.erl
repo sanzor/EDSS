0baf1fcf640a02460bfa54ef1092d01e2c2409c4
Adrian Bercovici <adriansan_93@yahoo.com>
2020-11-23 22:32:08 +0200

    commiting

--- src/wsup.erl
+++ src/wsup.erl
@@ -10,18 +10,11 @@ start()->
     supervisor:start_link({local,?NAME},?NAME,[]).
 
 init(Args)->
-    Options=[inet,]
-    ListenSock=gen_tcp:listen(?PORT,[]),
-    SupFlags=#{
-        strategy=>simple_one_for_one,
-        intensity=>5,
-        period=>1000
-        },
-    ChildSpec=#{
-        identity=>{worker,start,[ListenSock]},
-        restart=>transient,
-        shutdown=>brutal_kill,
-        type=>worker
-        },
-    {ok,{SupFlags,ChildSpec}}.
+    case gen_tcp:listen(?PORT,[]) of
+        {ok,L}->
+            SupFlags=#{strategy=>simple_one_for_one,intensity=>5,period=>1000},
+            ChildSpec=#{id=>worker,start=>{worker,start,[L]},restart=>transient,shutdown=>brutal_kill,type=>worker,modules=>[worker]},
+            {ok,{SupFlags,[ChildSpec]}};
+        Error->socket_error
+    end.
 
